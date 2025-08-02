fb1b736804882ff96ff63b672b0f26fbfe4c2de0
Prem Karhale <premkarhale383@gmail.com>
2025-08-02 13:29:53 +0530

    import githubInfoLoader from Github.jsx

--- Project04/src/main.jsx
+++ Project04/src/main.jsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Component/Home/Home.jsx'
+import About from './Component/About/About.jsx'
+import Contact from './Component/Contact/Contact.jsx'
+import Github, { githubInfoLoader } from './Component/Github/Github.jsx'
+
+const router =createBrowserRouter([{
+ 
+    path :"/",
+    element : <Layout/>,
+    children:[
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path:"about",
+        element: <About />
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      },
+      {
+        loader:githubInfoLoader,
+        path:"github",
+        element:<Github/>
+      }
+    ]
+  }
+]
+
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+
   </StrictMode>,
 )
