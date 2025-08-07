bf0da27873ece37093874e1992cbf70c075f8826
Prem Karhale <premkarhale383@gmail.com>
2025-08-07 22:53:14 +0530

    07 Project Todo list

---
+++ 07todolist/src/contexts/todo.js
@@ -0,0 +1,23 @@
+import { createContext,useContext } from "react";
+
+export const Todocontext = createContext({
+    todo:[
+        {
+            id:1,
+            todo : "Todo msg",
+            completed:false
+        }
+    ],
+    addTodo:(todo)=>{},
+    updateTodo: (id , todo)=>{},    //these are the functionalities !!! IN TODO'S
+    deleteTodo:(id)=>{},
+    Togglecomplete:(id)=>{}
+
+}); //created a new context !!
+
+//gives access to this context 
+export const useTodo = ()=>{
+    return useContext(Todocontext)
+}
+
+export const Todoprovider = Todocontext.Provider
