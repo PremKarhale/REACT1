e1d7d07b43594b4898a271dcf4dbc03a0dc05fe8
Prem Karhale <premkarhale383@gmail.com>
2025-08-27 20:54:48 +0530

    redux_toolkit Project 8

---
+++ ReduxToolkit/src/features/TodoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+//created one obj name initial state 
+const initialState = {
+    Todo: [{ id: 1, text: "hello world " }]    //this is my STATE
+}
+
+// function sayhello(){
+//     console.log("hello")
+// }
+export const todoSlice = createSlice({
+    name: "chai",
+    initialState,   //taken the reference of my state 
+    reducers: {
+        // addtodo: sayhello  // function yaha par bhi likh sakta hu or uska reference bhi de sakta hu !!! 
+        addtodo: (state, action) => {  // always get state or action ka access
+            const todo = {
+                id: nanoid(),      //todo create kiya
+                text: action.payload
+            }
+            state.Todo.push(todo)
+
+        },
+        removetodo: (state, action) => {
+            state.Todo = state.Todo.filter((todo) => todo.id != action.payload)
+        },
+        updatetodo: (state, action) => {
+            state.Todo = state.Todo.map((todo) => todo.id == action.payload.id ? { ...todo, text: action.payload.text } : todo)
+        }
+
+    }
+})
+
+export const { removetodo, updatetodo, addtodo } = todoSlice.actions
+export default todoSlice.reducer
