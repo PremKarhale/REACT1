caf17da63d4b91e5a402b56f175f13e3c37fb4bb
Prem Karhale <premkarhale383@gmail.com>
2025-08-28 00:47:07 +0530

    Added component Addtodo and todo in the jsx

--- ReduxToolkit/src/App.jsx
+++ ReduxToolkit/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './component/Todo'
+import Addtodo from './component/Addtodo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2>start learning reduxToolkit</h2>
+    <div className='text-4xl font-bold'>learn about redux toolKit</div>
+      <Addtodo />
+      <Todo />
     </>
   )
 }
