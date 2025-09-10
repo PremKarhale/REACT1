b46c1fa2db96ad012ead7dc6c2d4d8922e6e7500
Prem Karhale <premkarhale383@gmail.com>
2025-09-10 08:36:54 +0530

    appwrite  config functionalities to the database

---
+++ 12MegaBlog/src/appwrite/config.js
@@ -0,0 +1,133 @@
+//configuring the post from the databases (create , update , delete , preview etc ) in the databases !!
+
+import conf from '../conf/conf';
+import { Client, ID, Databases, Storage } from "appwrite";
+
+
+export class Service {
+    client = new Client()
+    databases;
+    bucket;
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwritedDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title, content, featuredImage, status, userId
+                }
+            )
+
+        } catch (error) {
+            console.log("Appwrite error :: error ", error)
+        }
+    }
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appwritedDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+
+        } catch (error) {
+            console.log("Appwrite service :: update post :: error", error)
+        }
+
+    }
+    async deletePost(slug) {
+        try {
+            return await this.databases.deleteDocument(
+                conf.appwritedDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: update post :: error", error)
+            return false
+        }
+
+    }
+    // to get only single searched posts!!
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.appwritedDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+
+        } catch (error) {
+            console.log("Appwrite service :: get post :: error", error)
+            return false
+        }
+    }
+    // to get all the active posts 
+    async getposts(queries = Query.equal("status","active")){
+        try {
+            return await this.databases.listDocuments(
+                  conf.appwritedDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+            
+        } catch (error) {
+            console.log("Appwrite service :: get post :: error", error)
+            return false
+            
+        }
+
+    }
+    // storge of the file 
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+              console.log("Appwrite service :: get post :: error", error)
+            return false            
+        }
+
+    }
+    async deleteFile(fileId){
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true 
+        } catch (error) {
+             console.log("Appwrite service :: get post :: error", error)
+            return false 
+        }
+    }
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+
+    }
+
+}
+const appwirteService = new Service()
+export default appwirteService
