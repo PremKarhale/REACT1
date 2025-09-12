bb042aa2eed1eae4550e13f9200e8a04a8bd5402
Prem Karhale <premkarhale383@gmail.com>
2025-09-12 12:49:48 +0530

    Main.jsx

---
+++ 12MegaBlog/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import store from './store/store.js'
+import { Provider } from 'react-redux'
+import React from 'react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
