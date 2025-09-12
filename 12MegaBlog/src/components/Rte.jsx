3b27e58a8ab0793a6b8e92129c87f29c20bb0c2e
Prem Karhale <premkarhale383@gmail.com>
2025-09-12 12:49:30 +0530

    Rte.jsx

--- 12MegaBlog/src/components/Rte.jsx
+++ 12MegaBlog/src/components/Rte.jsx
@@ -1,34 +1,53 @@
+//RTE (Real time editor )
+
+
 import React from 'react'
-import {Editor} from '@tinymce/tinymce-react'
+import { Editor } from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
 
 
-export default function Rte() {
-  return (
-<Editor
-    initialValue='default value'
-    init={{
-        branding: false,
-        height: 500,
-        menubar: false,
-        plugins: [
-            // Basic text formatting
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'help', 'wordcount',
-            
-            // Advanced formatting
-            'paste', 'directionality', 'emoticons', 'template', 'codesample',
-            'hr', 'pagebreak', 'nonbreaking', 'toc', 'imagetools',
-            
-            // Collaboration & workflow
-            'autosave', 'save', 'quickbars', 'accordion', 'autoresize'
-        ],
-        toolbar: 'undo redo | blocks | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
-    }}
+export default function Rte({ name, control, label, defaultValue = "" }) {
+return (
+<div className='w-full '>
+    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+    <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onchange } }) => {
+            {
+        <Editor
+            initialValue='default value'
+            init={{
+                branding: false,
+                height: 500,
+                menubar:true,
+                plugins: [
+                    // Basic text formatting
+                    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                    'insertdatetime', 'media', 'table', 'help', 'wordcount',
+
+                    // Advanced formatting
+                    'paste', 'directionality', 'emoticons', 'template', 'codesample',
+                    'hr', 'pagebreak', 'nonbreaking', 'toc', 'imagetools',
+
+                    // Collaboration & workflow
+                    'autosave', 'save', 'quickbars', 'accordion', 'autoresize'
+                ],
+                toolbar: 'undo redo | blocks | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
+            }}
+            onEditorChange={onchange}
+        />
+    }
+}
+}
 />
 
-  )
+        </div>
+
+
+    )
 }
 
 
